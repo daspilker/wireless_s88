a1acf65a97a23ccb31f9c53c87428dddbaa3ba4d
Daniel Spilker <mail@daniel-spilker.com>
2010-07-01 08:56:16 +0200

    more refactoring, -O3

--- main.c
+++ main.c
@@ -23,8 +23,6 @@
 #define LED_ON()  PORTD |=  _BV(PIN_LED)
 #define LED_OFF() PORTD &= ~_BV(PIN_LED)
 
-#define RF12FREQ(freq)	((freq-430.0)/0.0025)							// macro for calculating frequency value out of frequency in MHz
-
 #define USICR_CLOCK       _BV(USIWM0) | _BV(USITC)
 #define USICR_SHIFT_CLOCK _BV(USIWM0) | _BV(USITC) | _BV(USICLK)
 
@@ -46,29 +44,9 @@ static void rf12_trans(uint16_t value) {
   CHIP_SELECT_OFF();
 }
 
-static void rf12_setbandwidth(unsigned char bandwidth, unsigned char gain, unsigned char drssi) {
-  rf12_trans(0x9400|((bandwidth&7)<<5)|((gain&3)<<3)|(drssi&7));
-}
-
-static void rf12_setfreq(unsigned short freq) {
-  if (freq<96)				// 430,2400MHz
-    freq=96;
-  else if (freq>3903)			// 439,7575MHz
-    freq=3903;
-  rf12_trans(0xA000|freq);
-}
-
-static void rf12_setbaud(unsigned short baud) {
-  if (baud<663)
-    return;
-  if (baud<5400)					// Baudrate= 344827,58621/(R+1)/(1+CS*7)
-    rf12_trans(0xC680|((43104/baud)-1));
-  else
-    rf12_trans(0xC600|((344828UL/baud)-1));
-}
-
-static void rf12_setpower(unsigned char power, unsigned char mod) {	
-  rf12_trans(0x9800|(power&7)|((mod&15)<<4));
+static void rf12_ready(void) {
+  CHIP_SELECT_ON();
+  loop_until_bit_is_clear(PIND, PIN_IRQ);
 }
 
 static void rf12_init(void) {
@@ -76,29 +54,22 @@ static void rf12_init(void) {
   PORTB |= _BV(PIN_SEL);
   PORTD |= _BV(PIN_IRQ);
 
-  rf12_ready();
-  // loop_until_bit_is_clear(PIND, PIN_IRQ); // wait until POR done
+  loop_until_bit_is_clear(PIND, PIN_IRQ); // wait until POR done
   
-  rf12_trans(0xC0E0);			// AVR CLK: 10MHz
-  rf12_trans(0x80E7);			// Enable FIFO, 868MHz
-  rf12_trans(0xC2AB);			// Data Filter: internal
-  rf12_trans(0xCA81);			// Set FIFO mode
-  rf12_trans(0xE000);			// disable wakeuptimer
-  rf12_trans(0xC800);			// disable low duty cycle
-  rf12_trans(0xC4F7);			// AFC settings: autotuning: -10kHz...+7,5kHz
-
-  rf12_setfreq(RF12FREQ(433.92));	// Sende/Empfangsfrequenz auf 433,92MHz einstellen
-  rf12_setbandwidth(4, 1, 4);		// 200kHz Bandbreite, -6dB Verstärkung, DRSSI threshold: -79dBm 
-  rf12_setbaud(19200);			// 19200 baud
-  rf12_setpower(0, 6);			// 1mW Ausgangangsleistung, 120kHz Frequenzshift
+  rf12_trans(0xC0E0);	                  // AVR CLK: 10MHz
+  rf12_trans(0x80E7);			  // Enable FIFO, 868MHz
+  rf12_trans(0xC2AB);			  // Data Filter: internal
+  rf12_trans(0xCA81);			  // Set FIFO mode
+  rf12_trans(0xE000);			  // disable wakeuptimer
+  rf12_trans(0xC800);			  // disable low duty cycle
+  rf12_trans(0xC4F7);			  // AFC settings: autotuning: -10kHz...+7,5kHz
+  rf12_trans(0xA620);                     // Sende/Empfangsfrequenz auf 433,92MHz einstellen
+  rf12_trans(0x948C);                     // 200kHz Bandbreite, -6dB Verstärkung, DRSSI threshold: -79dBm 
+  rf12_trans(0xC610);                     // 19200 baud
+  rf12_trans(0x9860);                     // 1mW Ausgangangsleistung, 120kHz Frequenzshift
 }
 
-void rf12_ready(void) {
-  CHIP_SELECT_ON();
-  loop_until_bit_is_clear(PIND, PIN_IRQ);
-}
-
-void rf12_txdata(unsigned char *data, unsigned char number) {
+static void rf12_txdata(unsigned char *data, unsigned char number) {
   rf12_trans(0x8238);			// TX on
   rf12_ready();
   rf12_trans(0xB8AA);
@@ -143,4 +114,3 @@ int main(void) {
     _delay_ms(200);
   }
 }
-
